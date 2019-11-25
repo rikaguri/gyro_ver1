c155ed086ac03183466546f6dad02679c646c80f
rikaguri <kirarika7@gmail.com>
2019-11-25 14:55:46 +0900

    SDKを加えました！！！！

---
+++ gyro_ver1/Sample-Bridging-Header.h
@@ -0,0 +1,20 @@
+//
+//  Sample-Bridging-Header.h
+//  gyro_ver1
+//
+//  Created by Erika Ura on 2019/11/25.
+//  Copyright © 2019年 Erika Ura. All rights reserved.
+//
+
+//#ifndef FirstViewController_h
+//#define FirstViewController_h
+
+
+//#endif /* FirstViewController_h */
+#inport "/Users/erika/github/gyro_ver1/gyro_ver1/AsReaderDockSDK.framework"
+#define df_CELL_TAG_INFO         @"TAG_INFO"
+#define df_CELL_TAG_COUNT        @"COUNT"
+#define df_CELL_TAG_RAW          @"RAW"
+#define df_CELL_TAG_DEVICETYPE   @"DEVICE"
+#define df_CELL_TAG_RSSI         @"RSSI"
+
